eaaa23285c0398b0c9807fb8fd954a12998c6e46
Tyler Kopf <tyler@brainiumstudios.com>
2016-11-21 22:23:21 -0800

    test throwing an exception from a transaction

---
+++ test/exception.cpp
@@ -0,0 +1,39 @@
+#include <lstm/lstm.hpp>
+
+#include "simple_test.hpp"
+#include "thread_manager.hpp"
+
+using lstm::atomic;
+using lstm::var;
+
+static constexpr auto loop_count = LSTM_TEST_INIT(200000, 40000);
+
+int main() {
+    var<int> x{0};
+    
+    {
+        thread_manager tm;
+        
+        for (int i = 0; i < 5; ++i) {
+            tm.queue_thread([&] {
+                for (int j = 0; j < loop_count; ++j) {
+                     try {
+                        atomic([&](auto& tx) {
+                             int foo = tx.load(x);
+                             tx.store(x, foo + 5);
+                             if (foo + 5 >= 10000)
+                                 throw std::exception{};
+                        });
+                     } catch(const std::exception&) {
+                     }
+                }
+            });
+        }
+        
+        tm.run();
+    }
+    
+     CHECK(x.unsafe_load() == 10000 - 5);
+    
+    return test_result();
+}
