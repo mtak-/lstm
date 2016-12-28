59e168582c6ea58dc00bbc01f78eff6ee0d89ac2
Tyler Kopf <tyler@brainiumstudios.com>
2016-12-27 19:48:54 -0800

    todos and minor cleanup

--- include/lstm/memory.hpp
+++ include/lstm/memory.hpp
@@ -7,10 +7,23 @@ LSTM_BEGIN
     template<typename Alloc,
              typename AllocTraits = std::allocator_traits<Alloc>,
              typename Pointer = typename AllocTraits::pointer,
-        LSTM_REQUIRES_(!std::is_same<detail::uncvref<Alloc>, thread_data>{} &&
-                       !std::is_const<Alloc>{})>
+        LSTM_REQUIRES_(!std::is_const<Alloc>{})>
     inline Pointer allocate(Alloc& alloc, thread_data& tls_td = tls_thread_data())
     { return tls_td.allocate(alloc); }
+    
+    template<typename Alloc,
+             typename AllocTraits = std::allocator_traits<Alloc>,
+             typename Pointer = typename AllocTraits::pointer,
+        LSTM_REQUIRES_(!std::is_const<Alloc>{})>
+    inline Pointer allocate(Alloc& alloc,
+                            const std::size_t count,
+                            thread_data& tls_td = tls_thread_data())
+    { return tls_td.allocate(alloc, count); }
+    
+    // TODO: deallocate
+    //       construct,
+    //       destroy,
+    //       some kinda tx_safe_allocator
 LSTM_END
 
 #endif /* LSTM_MEMORY_HPP */
