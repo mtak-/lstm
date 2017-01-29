94248a090c41ecbbe9cb4718339d31ee2746abdc
tyler <tyler@brainiumstudios.com>
2017-01-28 22:45:50 -0800

    add asserts into write_set_lookup

--- include/lstm/detail/write_set_lookup.hpp
+++ include/lstm/detail/write_set_lookup.hpp
@@ -13,15 +13,23 @@ LSTM_DETAIL_BEGIN
         constexpr write_set_lookup(var_storage* const in_pending_write) noexcept
             : hash_(0)
             , pending_write_(in_pending_write)
-        {}
+        { assert(pending_write_ != nullptr); }
+        
         write_set_lookup(const hash_t in_hash) noexcept
             : hash_(in_hash)
-        {}
+        { assert(hash_ != 0); }
         
         inline constexpr bool success() const noexcept { return !hash_; }
-        inline constexpr var_storage& pending_write() const noexcept { return *pending_write_; }
-        inline constexpr hash_t& hash() noexcept { return hash_; }
-        inline constexpr hash_t hash() const noexcept { return hash_; }
+        
+        inline constexpr var_storage& pending_write() const noexcept {
+            assert(success());
+            return *pending_write_;
+        }
+        
+        inline constexpr hash_t hash() const noexcept {
+            assert(!success());
+            return hash_;
+        }
     };
 LSTM_DETAIL_END
 
