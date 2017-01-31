957bb018b0d66596117ec97fd34939671c15de8c
tyler <tyler@brainiumstudios.com>
2017-01-30 21:50:51 -0800

    turn clang-format off for some tricky code sections with preprocessor directives

--- include/lstm/detail/lstm_fwd.hpp
+++ include/lstm/detail/lstm_fwd.hpp
@@ -1,11 +1,13 @@
 #ifndef LSTM_DETAIL_LSTM_FWD_HPP
 #define LSTM_DETAIL_LSTM_FWD_HPP
 
+// clang-format off
 #ifdef NDEBUG
     #if NDEBUG != 1
         #error "NDEBUG must either be undefined or defined to be 1"
     #endif
 #endif
+// clang-format on
 
 #include <lstm/detail/compiler.hpp>
 
@@ -15,6 +17,7 @@
 #include <type_traits>
 #include <utility>
 
+// clang-format off
 #ifndef NDEBUG
     #define LSTM_BEGIN namespace lstm { inline namespace v1 { inline namespace debug {
     #define LSTM_END }}}
@@ -28,28 +31,27 @@
 
 #define LSTM_TEST_BEGIN LSTM_BEGIN namespace test {
 #define LSTM_TEST_END LSTM_END }
+// clang-format on
 
 // from rangev3
-#define LSTM_PP_CAT_(X, Y) X ## Y
-#define LSTM_PP_CAT(X, Y)  LSTM_PP_CAT_(X, Y)
+#define LSTM_PP_CAT_(X, Y) X##Y
+#define LSTM_PP_CAT(X, Y) LSTM_PP_CAT_(X, Y)
 
 #define LSTM_REQUIRES_(...)                                                                        \
-    int LSTM_PP_CAT(_concept_requires_, __LINE__) = 42,                                            \
-    typename std::enable_if<                                                                       \
-        (LSTM_PP_CAT(_concept_requires_, __LINE__) == 43) || (__VA_ARGS__),                        \
-        int                                                                                        \
-    >::type = 0                                                                                    \
-    /**/
-    
+    int LSTM_PP_CAT(_concept_requires_, __LINE__)                                                  \
+        = 42,                                                                                      \
+        typename std::enable_if < (LSTM_PP_CAT(_concept_requires_, __LINE__) == 43)                \
+            || (__VA_ARGS__),                                                                      \
+        int > ::type = 0 /**/
+
 #define LSTM_REQUIRES(...)                                                                         \
-    template<                                                                                      \
-        int LSTM_PP_CAT(_concept_requires_, __LINE__) = 42,                                        \
-        typename std::enable_if<                                                                   \
-            (LSTM_PP_CAT(_concept_requires_, __LINE__) == 43) || (__VA_ARGS__),                    \
-            int                                                                                    \
-        >::type = 0>                                                                               \
-    /**/
-    
+    template<int LSTM_PP_CAT(_concept_requires_, __LINE__) = 42,                                   \
+             typename std::enable_if<(LSTM_PP_CAT(_concept_requires_, __LINE__) == 43)             \
+                                         || (__VA_ARGS__),                                         \
+                                     int>::type                                                    \
+             = 0> /**/
+
+// clang-format off
 #ifdef LSTM_LOG_TRANSACTIONS
     #include <lstm/detail/transaction_log.hpp>
     
@@ -108,21 +110,22 @@
 #ifndef LSTM_SIGNED_LOCKFREE_WORD
     #define LSTM_SIGNED_LOCKFREE_WORD std::intptr_t;
 #endif
+// clang-format on
 
 LSTM_BEGIN
     // TODO verify lockfreeness of this on each platform
-    using word = LSTM_SIGNED_LOCKFREE_WORD;
+    using word  = LSTM_SIGNED_LOCKFREE_WORD;
     using uword = std::make_unsigned_t<word>;
-    
-    using gp_t = uword;
+
+    using gp_t       = uword;
     using mutex_type = std::mutex;
-    
+
     static_assert(std::is_integral<word>{}, "type chosen for word must be an integral type");
     static_assert(std::is_signed<word>{}, "type chosen for word must be signed");
-    
+
     template<typename T, typename Alloc = std::allocator<std::remove_reference_t<T>>>
     struct var;
-    
+
     struct transaction;
     struct transaction_domain;
     struct thread_data;
@@ -130,125 +133,153 @@ LSTM_END
 
 LSTM_DETAIL_BEGIN
     using var_storage = void*;
-    using hash_t = std::uint64_t;
-    
+    using hash_t      = std::uint64_t;
+
     struct read_write_fn;
     struct var_base;
-    
-    struct tx_retry {};
-    
+
+    struct tx_retry
+    {
+    };
+
     template<typename T>
     using uncvref = std::remove_cv_t<std::remove_reference_t<T>>;
-    
+
     template<typename... Bs>
-    using and_ = std::is_same<
-        std::integer_sequence<bool, Bs::value..., true>,
-        std::integer_sequence<bool, true, Bs::value...>>;
-    
+    using and_ = std::is_same<std::integer_sequence<bool, Bs::value..., true>,
+                              std::integer_sequence<bool, true, Bs::value...>>;
+
     template<typename Bool>
     using not_ = std::integral_constant<bool, !Bool::value>;
-    
+
     template<typename... Ts>
     struct all_same;
-    
+
     template<>
-    struct all_same<> : std::true_type {};
-    
+    struct all_same<> : std::true_type
+    {
+    };
+
     template<typename T, typename... Ts>
-    struct all_same<T, Ts...> : and_<std::is_same<T, Ts>...> {};
-    
+    struct all_same<T, Ts...> : and_<std::is_same<T, Ts>...>
+    {
+    };
+
     template<typename...>
     using void_ = void;
-    
+
     template<typename...>
-    struct list {};
-    
+    struct list
+    {
+    };
+
     template<template<typename...> class Trait, typename... Ts>
     std::false_type detector(long);
-    
+
     template<template<typename...> class Trait, typename... Ts>
     std::true_type detector(decltype(std::declval<Trait<Ts...>>(), 42));
-    
+
     template<template<typename...> class Trait, typename... Ts>
     using supports = decltype(lstm::detail::detector<Trait, Ts...>(42));
-    
+
     template<typename T>
     void implicit_constructor_impl(T);
-    
+
     template<typename To, typename... Froms>
-    using is_convertible_ =
-        decltype(lstm::detail::implicit_constructor_impl<To>({std::declval<Froms>()...}));
-        
+    using is_convertible_
+        = decltype(lstm::detail::implicit_constructor_impl<To>({std::declval<Froms>()...}));
+
     template<typename To, typename From, typename... Froms>
-    using is_ilist_uncvref_convertible =
-        decltype(lstm::detail::implicit_constructor_impl<To>(
-                    {std::initializer_list<uncvref<From>>{std::declval<From>(),
-                                                          std::declval<Froms>()...}}));
-    
+    using is_ilist_uncvref_convertible = decltype(lstm::detail::implicit_constructor_impl<To>(
+        {std::initializer_list<uncvref<From>>{std::declval<From>(), std::declval<Froms>()...}}));
+
     template<typename To, typename From, typename... Froms>
-    using is_ilist_const_convertible =
-        decltype(lstm::detail::implicit_constructor_impl<To>(
-                    {std::initializer_list<const uncvref<From>>{std::declval<From>(),
-                                                                std::declval<Froms>()...}}));
-    
+    using is_ilist_const_convertible = decltype(lstm::detail::implicit_constructor_impl<To>(
+        {std::initializer_list<const uncvref<From>>{std::declval<From>(),
+                                                    std::declval<Froms>()...}}));
+
     template<typename To, typename From, typename... Froms>
-    using is_ilist_volatile_convertible =
-        decltype(lstm::detail::implicit_constructor_impl<To>(
-                    {std::initializer_list<volatile uncvref<From>>{std::declval<From>(),
-                                                                   std::declval<Froms>()...}}));
-    
+    using is_ilist_volatile_convertible = decltype(lstm::detail::implicit_constructor_impl<To>(
+        {std::initializer_list<volatile uncvref<From>>{std::declval<From>(),
+                                                       std::declval<Froms>()...}}));
+
     template<typename To, typename From, typename... Froms>
-    using is_ilist_cv_convertible =
-        decltype(lstm::detail::implicit_constructor_impl<To>(
-                    {std::initializer_list<const volatile uncvref<From>>{std::declval<From>(),
-                                                                      std::declval<Froms>()...}}));
-    
+    using is_ilist_cv_convertible = decltype(lstm::detail::implicit_constructor_impl<To>(
+        {std::initializer_list<const volatile uncvref<From>>{std::declval<From>(),
+                                                             std::declval<Froms>()...}}));
+
     template<typename To, typename... Froms>
     struct is_convertible;
-    
+
     template<typename To>
-    struct is_convertible<To> : supports<is_convertible_, To> {};
-    
+    struct is_convertible<To> : supports<is_convertible_, To>
+    {
+    };
+
     template<typename To, typename From>
-    struct is_convertible<To, From> : std::is_convertible<From, To> {};
-    
+    struct is_convertible<To, From> : std::is_convertible<From, To>
+    {
+    };
+
     template<typename To, typename From0, typename From1, typename... FromTail>
     struct is_convertible<To, From0, From1, FromTail...>
         : and_<supports<is_convertible_, To, From0, From1, FromTail...>,
                std::integral_constant<bool,
-                    !all_same<uncvref<From0>, uncvref<From1>, uncvref<FromTail>...>{} ||
-                    (!supports<is_ilist_uncvref_convertible, To, From0, From1, FromTail...>{} &&
-                     !supports<is_ilist_const_convertible, To, From0, From1, FromTail...>{} &&
-                     !supports<is_ilist_volatile_convertible, To, From0, From1, FromTail...>{} &&
-                     !supports<is_ilist_cv_convertible, To, From0, From1, FromTail...>{})>>
-    {};
-    
+                                      !all_same<uncvref<From0>,
+                                                uncvref<From1>,
+                                                uncvref<FromTail>...>{}
+                                          || (!supports<is_ilist_uncvref_convertible,
+                                                        To,
+                                                        From0,
+                                                        From1,
+                                                        FromTail...>{}
+                                              && !supports<is_ilist_const_convertible,
+                                                           To,
+                                                           From0,
+                                                           From1,
+                                                           FromTail...>{}
+                                              && !supports<is_ilist_volatile_convertible,
+                                                           To,
+                                                           From0,
+                                                           From1,
+                                                           FromTail...>{}
+                                              && !supports<is_ilist_cv_convertible,
+                                                           To,
+                                                           From0,
+                                                           From1,
+                                                           FromTail...>{})>>
+    {
+    };
+
     template<typename Func, typename Tx>
     using callable_with_tx_ = decltype(std::declval<Func>()(std::declval<Tx>()));
-    
+
     template<typename Func, typename Tx>
     using callable_with_tx = supports<callable_with_tx_, Func, Tx>;
-    
+
     template<typename Func>
     using callable_ = decltype(std::declval<Func>()());
-    
+
     template<typename Func>
     using callable = supports<callable_, Func>;
-    
-    template<typename Func, bool = callable_with_tx<Func&, transaction&>{}(),
-                            bool = callable<Func&>{}()>
+
+    template<typename Func,
+             bool = callable_with_tx<Func&, transaction&>{}(),
+             bool = callable<Func&>{}()>
     struct transact_result_impl;
 
     template<typename Func, bool b>
-    struct transact_result_impl<Func, true, b> {
+    struct transact_result_impl<Func, true, b>
+    {
         using type = decltype(std::declval<Func&>()(std::declval<transaction&>()));
-        static constexpr bool nothrow =
-            noexcept(std::declval<Func&>()(std::declval<transaction&>()));
+        static constexpr bool nothrow
+            = noexcept(std::declval<Func&>()(std::declval<transaction&>()));
     };
-    
+
     template<typename Func>
-    struct transact_result_impl<Func, false, true> {
-        using type = decltype(std::declval<Func&>()());
+    struct transact_result_impl<Func, false, true>
+    {
+        using type                    = decltype(std::declval<Func&>()());
         static constexpr bool nothrow = noexcept(std::declval<Func&>()());
     };
 
@@ -259,35 +290,47 @@ LSTM_DETAIL_BEGIN
     constexpr bool transact_nothrow = transact_result_impl<Func>::nothrow;
 
     template<typename Func, typename = void>
-    struct is_void_transact_function : std::false_type {};
+    struct is_void_transact_function : std::false_type
+    {
+    };
 
     template<typename Func>
     struct is_void_transact_function<Func, std::enable_if_t<std::is_void<transact_result<Func>>{}>>
-        : std::true_type {};
-        
+        : std::true_type
+    {
+    };
+
     template<typename Func>
     using is_transact_function = supports<transact_result, Func>;
-    
+
     template<typename Func, typename = void>
-    struct is_nothrow_transact_function : std::false_type {};
+    struct is_nothrow_transact_function : std::false_type
+    {
+    };
 
     template<typename Func>
     struct is_nothrow_transact_function<Func, std::enable_if_t<transact_nothrow<Func>>>
-        : std::true_type {};
+        : std::true_type
+    {
+    };
 
     template<typename T>
     using uninitialized = std::aligned_storage_t<sizeof(T), alignof(T)>;
-    
-// http://stackoverflow.com/questions/17864256/allocator-traitsconstruct-vs-allocator-traitsallocate
-    template<typename T> inline T* to_raw_pointer(T* p) noexcept { return p; }
-    template<typename Pointer,
-        LSTM_REQUIRES_(!std::is_pointer<uncvref<Pointer>>())>
-    inline typename std::pointer_traits<uncvref<Pointer>>::element_type*
-    to_raw_pointer(Pointer&& p) noexcept {
-        return p != nullptr
-            ? detail::to_raw_pointer(p.operator->())
-            : nullptr;
+
+    template<typename T>
+    inline T* to_raw_pointer(T * p) noexcept
+    {
+        return p;
     }
+
+    // clang-format off
+    template<typename Pointer, LSTM_REQUIRES_(!std::is_pointer<uncvref<Pointer>>())>
+    inline typename std::pointer_traits<uncvref<Pointer>>::element_type*
+    to_raw_pointer(Pointer&& p) noexcept
+    {
+        return p != nullptr ? detail::to_raw_pointer(p.operator->()) : nullptr;
+    }
+// clang-format on
 LSTM_DETAIL_END
 
 LSTM_TEST_BEGIN
