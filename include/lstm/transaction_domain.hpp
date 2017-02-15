00a9a6ca9e8202bce5d0ba1ebf7772f3b257604e
tyler <tyler@brainiumstudios.com>
2017-02-14 20:11:18 -0800

    more explicit construction, and add missing const

--- include/lstm/transaction_domain.hpp
+++ include/lstm/transaction_domain.hpp
@@ -12,14 +12,18 @@ LSTM_BEGIN
     private:
         static constexpr gp_t max_version = ~(gp_t(1) << (sizeof(gp_t) * 8 - 1));
 
-        LSTM_CACHE_ALIGNED std::atomic<gp_t> clock{0};
+        LSTM_CACHE_ALIGNED std::atomic<gp_t> clock;
 
     public:
-        inline constexpr transaction_domain() noexcept = default;
-        transaction_domain(const transaction_domain&)  = delete;
+        inline constexpr transaction_domain() noexcept
+            : clock{0}
+        {
+        }
+
+        transaction_domain(const transaction_domain&) = delete;
         transaction_domain& operator=(const transaction_domain&) = delete;
 
-        inline gp_t get_clock() noexcept { return clock.load(LSTM_RELAXED); }
+        inline gp_t get_clock() const noexcept { return clock.load(LSTM_RELAXED); }
 
         // returns the previous version
         inline gp_t fetch_and_bump_clock() noexcept
