db357a064ef78d348b0aa977a2f23eb7e24a9011
tyler <tyler@brainiumstudios.com>
2017-02-23 18:57:44 -0800

    add the ability to do untracked reads. this is useful in scenarios like a rbtree's lookup phase before insertion

--- include/lstm/transaction.hpp
+++ include/lstm/transaction.hpp
@@ -27,8 +27,9 @@ LSTM_BEGIN
                     tls_td->read_set.emplace_back(&src_var);
                     return result;
                 }
-            } else if (rw_valid(src_var))
+            } else if (rw_valid(src_var)) {
                 return iter->pending_write();
+            }
 
             detail::internal_retry();
         }
@@ -76,6 +77,35 @@ LSTM_BEGIN
                 return atomic_write_slow_path(dest_var, storage);
 
             tls_td->add_write_set_unchecked(dest_var, storage, hash);
+        }
+
+        LSTM_NOINLINE detail::var_storage
+        untracked_read_impl_slow_path(const detail::var_base& src_var) const
+        {
+            const thread_data::write_set_t::const_iterator iter = tls_td->write_set.find(src_var);
+            if (iter == tls_td->write_set.end()) {
+                const detail::var_storage result = src_var.storage.load(LSTM_ACQUIRE);
+                if (rw_valid(src_var.version_lock.load(LSTM_ACQUIRE)))
+                    return result;
+            } else if (rw_valid(src_var)) {
+                return iter->pending_write();
+            }
+
+            detail::internal_retry();
+        }
+
+        LSTM_ALWAYS_INLINE detail::var_storage
+        untracked_read_impl(const detail::var_base& src_var) const
+        {
+            assert(valid());
+
+            if (LSTM_UNLIKELY(tls_td->write_set.filter() & dumb_pointer_hash(src_var)))
+                return untracked_read_impl_slow_path(src_var);
+
+            const detail::var_storage result = src_var.storage.load(LSTM_ACQUIRE);
+            if (LSTM_LIKELY(rw_valid(src_var.version_lock.load(LSTM_ACQUIRE))))
+                return result;
+            return untracked_read_impl_slow_path(src_var);
         }
 
     public:
@@ -177,6 +207,24 @@ LSTM_BEGIN
             atomic_write_impl(dest_var, dest_var.allocate_construct((U &&) u));
         }
 
+        template<typename T, typename Alloc, LSTM_REQUIRES_(!var<T, Alloc>::atomic)>
+        LSTM_NOINLINE const T& untracked_read(const var<T, Alloc>& src_var) const
+        {
+            static_assert(std::is_reference<decltype(
+                              var<T, Alloc>::load(src_var.storage.load()))>{},
+                          "");
+            return var<T, Alloc>::load(untracked_read_impl(src_var));
+        }
+
+        template<typename T, typename Alloc, LSTM_REQUIRES_(var<T, Alloc>::atomic)>
+        LSTM_ALWAYS_INLINE T untracked_read(const var<T, Alloc>& src_var) const
+        {
+            static_assert(!std::is_reference<decltype(
+                              var<T, Alloc>::load(src_var.storage.load()))>{},
+                          "");
+            return var<T, Alloc>::load(untracked_read_impl(src_var));
+        }
+
         // reading/writing an rvalue probably never makes sense
         template<typename T, typename Alloc>
         void read(var<T, Alloc>&& v) const = delete;
@@ -186,6 +234,10 @@ LSTM_BEGIN
         void write(var<T, Alloc>&& v) const = delete;
         template<typename T, typename Alloc>
         void write(const var<T, Alloc>&& v) const = delete;
+        template<typename T, typename Alloc>
+        void untracked_read(var<T, Alloc>&& v) const = delete;
+        template<typename T, typename Alloc>
+        void untracked_read(const var<T, Alloc>&& v) const = delete;
     };
 LSTM_END
 
