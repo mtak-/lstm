dceb7a40149ac1b67c230ee0afb2ff22f736d115
tyler <tyler@brainiumstudios.com>
2017-03-12 01:05:41 -0800

    insert test

--- test/rbtree_tests/rbtree.cpp
+++ test/rbtree_tests/rbtree.cpp
@@ -8,10 +8,13 @@
 
 using tree_t = lstm::rbtree<int, int, std::less<>, mallocator<std::pair<int, int>>>;
 
-static constexpr int elem_count   = LSTM_TEST_INIT(1000000, 1000);
-static constexpr int loop_count   = LSTM_TEST_INIT(1, 1);
-static constexpr int thread_count = 8;
+static constexpr int   iter_count   = LSTM_TEST_INIT(10000000, 1000);
+static constexpr int   elem_count   = LSTM_TEST_INIT(65536, 1000);
+static constexpr int   loop_count   = LSTM_TEST_INIT(1, 1);
+static constexpr int   thread_count = 8;
+static constexpr float update_rate  = 1.0f;
 static_assert(elem_count % thread_count == 0, "");
+static_assert(iter_count % thread_count == 0, "");
 
 static LSTM_NOINLINE auto get_data()
 {
@@ -27,7 +30,7 @@ static LSTM_NOINLINE auto get_data()
     return data;
 }
 
-static auto data = get_data();
+static const auto data = get_data();
 
 static void populate_tree(tree_t& intmap)
 {
@@ -38,7 +41,7 @@ static void populate_tree(tree_t& intmap)
             manager.queue_thread([&intmap, t] {
                 lstm::thread_data& tls_td = lstm::tls_thread_data();
                 for (int i = 0; i < elem_count / thread_count; ++i) {
-                    lstm::atomic(tls_td, [&intmap, &tls_td, i, t](const lstm::transaction tx) {
+                    lstm::atomic(tls_td, [&intmap, i, t](const lstm::transaction tx) {
                         intmap.emplace(tx,
                                        data[i + t * elem_count / thread_count],
                                        data[i + t * elem_count / thread_count + elem_count]);
@@ -69,8 +72,11 @@ static void depopulate_tree(tree_t& intmap)
             manager.queue_thread([&intmap, t] {
                 lstm::thread_data& tls_td = lstm::tls_thread_data();
                 for (int i = elem_count / thread_count - 1; i >= 0; --i) {
-                    lstm::atomic(tls_td, [&intmap, &tls_td, i, t](const lstm::transaction tx) {
-                        intmap.erase_one(tx, data[i + t * elem_count / thread_count]);
+                    lstm::atomic(tls_td, [&intmap, i, t](const lstm::transaction tx) {
+                        const bool result
+                            = intmap.erase_one(tx, data[i + t * elem_count / thread_count]);
+                        (void)result;
+                        LSTM_ASSERT(result);
                     });
                 }
             });
@@ -97,11 +103,32 @@ static void update(tree_t& intmap)
         for (int t = 0; t < thread_count; ++t) {
             manager.queue_thread([&intmap, t] {
                 lstm::thread_data& tls_td = lstm::tls_thread_data();
-                for (int i = elem_count / thread_count - 1; i >= 0; --i) {
-                    // if ((i % 128) / 128.f < update_rate)
-                    lstm::atomic(tls_td, [&intmap, &tls_td, i, t](const lstm::transaction tx) {
-                        intmap.erase_one(tx, data[i + t * elem_count / thread_count]);
-                    });
+                for (int k = 0; k < iter_count / thread_count; ++k) {
+                    const int   i    = k % (elem_count / thread_count);
+                    const float perc = (k % 128) / 128.f;
+                    if (perc < update_rate) {
+                        lstm::atomic(tls_td, [&intmap, i, t](const lstm::transaction tx) {
+                            const bool result
+                                = intmap.erase_one(tx, data[i + t * (elem_count / thread_count)]);
+                            (void)result;
+                            LSTM_ASSERT(result);
+                        });
+                        lstm::atomic(tls_td, [&intmap, i, t](const lstm::transaction tx) {
+                            intmap.emplace(tx,
+                                           data[i + t * (elem_count / thread_count)],
+                                           data[i + t * (elem_count / thread_count) + elem_count]);
+                        });
+                    } else {
+                        const auto node
+                            = lstm::atomic(tls_td,
+                                           [&intmap, i, t](const lstm::read_transaction tx) {
+                                               return intmap
+                                                   .find(tx,
+                                                         data[i + t * elem_count / thread_count]);
+                                           });
+                        if (!node)
+                            std::terminate();
+                    }
                 }
             });
         }
@@ -109,7 +136,7 @@ static void update(tree_t& intmap)
         auto start = std::chrono::high_resolution_clock::now();
         manager.run();
         auto elapsed = std::chrono::high_resolution_clock::now() - start;
-        std::cout << "Delete Elapsed: "
+        std::cout << "Update Elapsed: "
                   << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()
                          / 1000000.f
                   << "s" << std::endl;
@@ -124,6 +151,7 @@ int main()
     for (int loop = 0; loop < loop_count; ++loop) {
         tree_t intmap;
         populate_tree(intmap);
+        update(intmap);
         depopulate_tree(intmap);
     }
 
